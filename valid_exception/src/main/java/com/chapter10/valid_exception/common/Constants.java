73ab424aae7a931a8c6b1c54b1eca881a207d637
정다교 <hyunn0121@duksung.ac.kr>
2023-12-19 15:46:04 +0900

    Feat : Week10 - 10 유효성 검사와 예외처리

---
+++ valid_exception/src/main/java/com/chapter10/valid_exception/common/Constants.java
@@ -0,0 +1,26 @@
+package com.chapter10.valid_exception.common;
+
+public class Constants {
+
+    public enum ExceptionClass {
+
+        PRODUCT("Product");
+
+        private String exceptionClass;
+
+        ExceptionClass(String exceptionClass) {
+            this.exceptionClass = exceptionClass;
+        }
+
+        public String getExceptionClass() {
+            return exceptionClass;
+        }
+
+        @Override
+        public String toString() {
+            return getExceptionClass() + " Exception. ";
+        }
+
+    }
+
+}
